9cde7d042a1530e2a09156f79732c1998248967b
Ryan Kennedy <rkennedy@yammer-inc.com>
2013-08-12 11:00:47 -0700

    Remove non-Ruby bits.

--- telemetry-agent/src/main/java/com/yammer/telemetry/agent/handlers/ClassInstrumentationHandler.java
+++
@@ -1,8 +0,0 @@
-package com.yammer.telemetry.agent.handlers;
-
-import javassist.ClassPool;
-import javassist.CtClass;
-
-public interface ClassInstrumentationHandler {
-    boolean transformed(CtClass cc, ClassPool pool);
-}
