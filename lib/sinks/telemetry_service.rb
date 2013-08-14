14dc0d60e674e099ed47cfe4fc49ac463ffe73bb
Ryan Kennedy <rkennedy@yammer-inc.com>
2013-08-13 21:34:45 -0700

    Add a little bit of debugging to see if we can figure out why the trace ID always starts at zero.

--- lib/sinks/telemetry_service.rb
+++ lib/sinks/telemetry_service.rb
@@ -9,6 +9,7 @@ module Telemetry
 
     # Record the span.
     def record(span)
+      Rails.logger.debug "Logging span: #{span.inspect}"
       @http.post('/spans',
                  Yajl::Encoder.encode(span),
                  {'Content-Type' => 'application/json', 'Accept' => 'application/json'})
@@ -16,6 +17,7 @@ module Telemetry
 
     # Record the annotation.
     def record_annotation(trace_id, id, annotation_data)
+      Rails.logger.debug "Logging annotation: #{annotation_data.inspect}"
       @http.post("/spans/#{trace_id}/#{id}",
                  Yajl::Encoder.encode(annotation_data),
                  {'Content-Type' => 'application/json', 'Accept' => 'application/json'})
