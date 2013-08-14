a8c0917c8e0c2ffb8440aaff99a643e7b0c398ea
Ryan Kennedy <rkennedy@yammer-inc.com>
2013-08-14 14:18:41 -0700

    Delegate was a bust…alias_method it is.

--- lib/instrumentation/zephyr.rb
+++ lib/instrumentation/zephyr.rb
@@ -1,6 +1,8 @@
-class InstrumentedZephyr < DelegateClass(Zephyr)
+class Zephyr
+  alias_method :old_perform, :perform
+
   def perform(method, path_components, headers, expect, timeout, data=nil)
     Rails.logger.error "Holy shit, instrumenting zephyr!"
-    super.perform(method, path_components, headers, expect, timeout, data=nil)
+    old_perform(method, path_components, headers, expect, timeout, data)
   end
 end
