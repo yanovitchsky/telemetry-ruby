15ab7c6d46c15732c0fa7cb4ccd0a9848a64ce9a
Ryan Kennedy <rkennedy@yammer-inc.com>
2013-08-14 14:57:32 -0700

    Generate and log the span and add annotations for Zephyr calls.

--- lib/instrumentation/zephyr.rb
+++ lib/instrumentation/zephyr.rb
@@ -2,7 +2,17 @@ class Zephyr
   alias_method :old_perform, :perform
 
   def perform(method, path_components, headers, expect, timeout, data=nil)
-    Rails.logger.error "Holy shit, instrumenting zephyr!"
-    old_perform(method, path_components, headers, expect, timeout, data)
+    span = Telemetry::Span.start_span(uri(path_components))
+    span.add_annotation('ClientSent')
+
+    begin
+      old_perform(method, path_components, headers, expect, timeout, data)
+    rescue Exception => e
+      span.add_annotation('ClientException', e.class.name)
+      raise
+    ensure
+      span.add_annotation('ClientReceived')
+      span.end
+    end
   end
 end
