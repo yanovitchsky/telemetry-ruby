2210a7bb6c201df5ac4e17ac7a2adbf5ab3a2625
Ryan Kennedy <rkennedy@yammer-inc.com>
2013-08-13 22:31:55 -0700

    Catch exceptions, log them, and then drop them when spans and annotations are being beaconed back to the collector service.

--- lib/sinks/telemetry_service.rb
+++ lib/sinks/telemetry_service.rb
@@ -9,18 +9,24 @@ module Telemetry
 
     # Record the span.
     def record(span)
-      Rails.logger.debug "Logging span: #{span.inspect}"
-      @http.post('/spans',
-                 Yajl::Encoder.encode(span),
-                 {'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+      begin
+        @http.post('/spans',
+                   Yajl::Encoder.encode(span),
+                   {'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+      rescue Exception => e
+        Rails.logger.info "Error logging span: #{e}"
+      end
     end
 
     # Record the annotation.
     def record_annotation(trace_id, id, annotation_data)
-      Rails.logger.debug "Logging annotation: #{annotation_data.inspect}"
-      @http.post("/spans/#{trace_id}/#{id}",
-                 Yajl::Encoder.encode(annotation_data),
-                 {'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+      begin
+        @http.post("/spans/#{trace_id}/#{id}",
+                   Yajl::Encoder.encode(annotation_data),
+                   {'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+      rescue Exception => e
+        Rails.logger.info "Error logging annotation: #{e}"
+      end
     end
   end
 end
