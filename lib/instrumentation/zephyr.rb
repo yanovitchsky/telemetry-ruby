4ac3982da0d7b1db52781b97b9249e4a5288d382
Ryan Kennedy <rkennedy@yammer-inc.com>
2013-08-14 12:11:03 -0700

    Switch from monkey patch to delegate to see if we can get super.perform() back.

--- lib/instrumentation/zephyr.rb
+++ lib/instrumentation/zephyr.rb
@@ -1,4 +1,4 @@
-class Zephyr
+class InstrumentedZephyr < DelegateClass(Zephyr)
   def perform(method, path_components, headers, expect, timeout, data=nil)
     Rails.logger.error "Holy shit, instrumenting zephyr!"
     super.perform(method, path_components, headers, expect, timeout, data=nil)
